4bfafc527ccf05e16ef891d2040108da4cebe478
jmalone801 <jmalone801@gmail.com>
2021-10-17 21:22:42 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1. Print Odds 1-20
+for(var i = 1; i <= 20; i+=2) {
+    console.log(i);
+}
+
+// 2. Decreasing Multiples of 3
+for(var i = 100; i >= 0; i--) {
+    if (i % 3 == 0)
+    console.log(i);
+}
+
+// 3. Print the sequence
+var array = [4, 2.5, 1, -0.5, -2, -3.5]
+for(var i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+// 4. Sigma
+var sum = 0; {
+for(var i = 0; i <= 100; i++) {
+    sum += i;
+    }
+    console.log(sum);
+}
+
+// 5. Factorial
+var product = 1; {
+for(var i = 1; i <= 12; i++) {
+    product *= i;
+    }
+    console.log(product);
+    }
